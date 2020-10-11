6b2cb117c421c07a57aa9f7da1f79b3cc1822cfb
timoweber <timo_weber@outlook.com>
2020-10-11 17:20:29 +0200

    #1: code cleanup

--- src/main/java/de/oneaxis/backend/symptom/SymptomId.java
+++ src/main/java/de/oneaxis/backend/symptom/SymptomId.java
@@ -1,9 +1,11 @@
 package de.oneaxis.backend.symptom;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import de.oneaxis.backend.ddd.ValueObject;
 
 import java.util.UUID;
 
+@ValueObject
 final class SymptomId {
     public final String value;
 

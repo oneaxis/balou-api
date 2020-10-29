5fcf8511e244a77df64cc192c6aaf69b4d14fcfb
timoweber <timo_weber@outlook.com>
2020-10-29 16:24:16 +0100

    #1: code cleanup

--- src/main/java/de/oneaxis/backend/domain/tactical/storytelling/AnimalCharacteristicsValidationService.java
+++ src/main/java/de/oneaxis/backend/domain/tactical/storytelling/AnimalCharacteristicsValidationService.java
@@ -1,7 +1,5 @@
 package de.oneaxis.backend.domain.tactical.storytelling;
 
-import org.apache.logging.log4j.util.Strings;
-
 import java.util.Objects;
 
 public class AnimalCharacteristicsValidationService implements ValidationService<AnimalCharacteristics> {
@@ -10,8 +8,6 @@ public class AnimalCharacteristicsValidationService implements ValidationService
         // Main instance validation
         if (!Objects.nonNull(validationSubject))
             throw new ValidationException("The provided instance of type AnimalCharacteristics has not been initialized!");
-        if (!Strings.isNotEmpty(validationSubject.getRace().getValue()))
-            throw new ValidationException("The provided instance of type Animal has no identity associated");
 
         // Nested instance validation
         var raceValidationService = new RaceValidationService();
